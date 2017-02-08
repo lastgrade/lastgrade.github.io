964fd05eb922f0d74175e4f1d8d90bae0b99bf72
lastgrade <lastgrade@outlook.com>
2017-02-08 21:49:04 +0530

    beta version

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+//var cssnano = require('gulp-cssnano');
+
+gulp.task('styles', function() {
+	gulp.src('./scss/*.scss')
+	  .pipe(sass({
+		outputStyle: 'compressed',
+		includePaths: ['bower_components/foundation/scss']
+	  }))
+	  .on('error', sass.logError)
+	  .pipe(gulp.dest('./css'));		
+});
+
+
+gulp.task('javascript', function () {
+   gulp.src(['./bower_components/jquery/dist/*.js','./bower_components/modernizr/*.js','./bower_components/foundation/js/*.js'])      
+      .pipe(gulp.dest('./js'));
+});
+
+
+
+gulp.task('default', function() {
+    return gulp.src('./css/*.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('./css'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('./scss/*.scss', ['styles']);
+});
+
